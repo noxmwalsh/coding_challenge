d0ec413e7f7f6cd3a2937de5e9532e1ebee8114a
Michael Walsh <noxiousthing@gmail.com>
2025-04-10 12:11:21 -0700

    JIRA-XXX Use Postgres instead of sqlite
    * Use Postgres adapter and PostgreSQL
    * Update database.yml to gitignore and create example file

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_04_10_175714) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,7 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_175714) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
