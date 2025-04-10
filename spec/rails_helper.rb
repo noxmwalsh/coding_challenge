4749b10e32c5ed9605a630232de36257efbc7b0b
Michael Walsh <noxiousthing@gmail.com>
2025-04-10 15:11:10 -0700

    JIRA-XXX Add shoulda matchers back in
    * Add shoulda matcher back in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'factory_bot_rails'
+require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -71,4 +72,12 @@ RSpec.configure do |config|
 
   # Add FactoryBot configuration
   config.include FactoryBot::Syntax::Methods
+end
+
+# Configure shoulda-matchers
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
