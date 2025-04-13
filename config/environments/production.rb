9df3dc4856beea56ddf90248070283616d69a77c
Michael Walsh <noxiousthing@gmail.com>
2025-04-13 15:46:27 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
 
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
